208e007f60bb8b935ff539d61726f8b8a0f42a92
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-18 17:17:36 -0300

    Add pw2 em properties e Injetado no codigo

--- src/main/java/dev/amf/config.java
+++ src/main/java/dev/amf/config.java
@@ -5,12 +5,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello")
 public class config {
-
+    @ConfigProperty(name = "pw2",  defaultValue="" )
+    String pw2;
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return pw2 + "from RESTEasy Reactive";
     }
 }
