7e2b2b3565c8db79bc751024d813dad8ba33afb6
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-18 17:15:34 -0300

    Projeto configuração YAML de PW2

---
+++ src/test/java/dev/amf/configIT.java
@@ -0,0 +1,9 @@
+package dev.amf;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class configIT extends configTest {
+
+    // Execute the same tests but in native mode.
+}
